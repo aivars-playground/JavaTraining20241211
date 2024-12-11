aae9d0e8a0debf91ebbcf364c0eecf54ed6490b2
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-11 19:33:15 +0000

    implementing api call

--- src/main/java/org/example/service/CourseRetrieverService.java
+++ src/main/java/org/example/service/CourseRetrieverService.java
@@ -1,7 +1,33 @@
 package org.example.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CourseRetrieverService {
+
+    private static final String PS_URI = "https://app.pluralsight.com/profile/data/author/%s/all-content";
+
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+
     public String retrieveCourse(String courseId) {
-        return null;
+
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create(String.format(PS_URI, courseId)))
+                .GET()
+                .build();
+
+        HttpResponse.BodyHandler<String> bodyHandler = HttpResponse
+                .BodyHandlers
+                .ofString();
+
+        try {
+            HttpResponse<String> response = CLIENT.send(request, bodyHandler);
+            return response.body();
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException("could not call API",ex);
+        }
     }
 }
