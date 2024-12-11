4cb048b2a43500eaa627dcb5a46c94bbf42d746c
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-11 23:19:29 +0000

    adding repository logic

--- course-info-repository/src/test/java/org/example/domain/CourseTest.java
+++ course-info-repository/src/test/java/org/example/domain/CourseTest.java
@@ -2,6 +2,8 @@ package org.example.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -9,18 +11,18 @@ class CourseTest {
 
     @Test
     void constructorWithValidation_success() {
-        Course course = new Course("id","title", 11, "uri://");
+        Course course = new Course("id","title", 11, "uri://", Optional.empty());
     }
 
     @Test
     void constructorWithValidation_exception_null() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Course(null,"title", 11, "uri://"));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Course(null,"title", 11, "uri://", Optional.empty()));
         assertEquals("invalid field value", exception.getMessage());
     }
 
     @Test
     void constructorWithValidation_exception_blank() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Course("   ","title", 11, "uri://"));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Course("   ","title", 11, "uri://", Optional.empty()));
         assertEquals("invalid field value", exception.getMessage());
     }
 
