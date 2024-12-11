d49b00a4bd64fba936811d57fa8192556394cf77
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-11 17:52:44 +0000

    adding error handling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length <1) {
+            System.out.println("please provide an author name");
+            return;
+        }
+
+        try{
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void retrieveCourses(String arg) {
+        System.out.println("Retrieving courses for:" + arg);
     }
 }
