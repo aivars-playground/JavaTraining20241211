6690f5c8b8e8f065ee4a14e281257066252409a9
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-11 18:25:29 +0000

    adding error logger, (missing implementation)

---
+++ src/main/java/org/example/CourseRetriever.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting Course Retriever");
+        if (args.length <1) {
+            LOGGER.warn("please provide an author name");
+            return;
+        }
+
+        try{
+            retrieveCourses(args[0]);
+        } catch (Exception ex) {
+            LOGGER.error("Unexpected error", ex);
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        LOGGER.info("Retrieving courses for authorId: '{}'", authorId);
+    }
+}
